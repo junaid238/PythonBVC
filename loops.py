1e5f203101b178e897b4f2b056000f016910d902
pwa <pwa@PWAHYD-8009.processw.local>
2018-02-20 12:06:19 +0530

    Loops docx and PY file

---
+++ loops.py
@@ -0,0 +1,54 @@
+# for x in range(10):
+# 	print(x)
+# for y in range(0,13):
+# 	print(y)
+# for x in range(10,100,5):
+# 	print(x)
+# #reverse / decrementing
+# for y in range(30,13,-1):
+# 	print(y)
+
+# nme = "python and data sceince"
+# print(len(nme))
+# for index in range(0,len(nme)):
+# 	print(nme[index])
+
+
+# for letters in nme:
+# 	print (letters)
+
+# for x in range(20,30):
+# 	pass
+
+# for i in range(0,30):
+# 	print(i)
+# 	if(i==12):
+# 		break
+
+# for i in range(0,30):
+# 	if(i==12):
+# 		continue
+# 	print(i)
+
+# s = "vinay"
+# for ch in s:
+# 	print(ch)
+# 	if(ch == 'a'):
+# 		break
+# for ch in s:
+# 	if(ch == 'n'):
+# 		continue
+# 	print(ch)
+
+# a = 10
+
+# while(a<100):
+# 	print(a)
+# 	a = a+1
+
+# while(True):
+# 	print('a')
+
+# b = 20 
+# while (b==20):
+# 	print(b)
