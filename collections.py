fcd7a91dedcad579ff15d9f04d94afedeacfcbfb
khan <khan@PWAHYD-8009.processw.local>
2018-02-26 11:33:22 +0530

    Lists

---
+++ collections.py
@@ -0,0 +1,74 @@
+names = ["Python" , 101 , 19 , 13 , "february"]
+nums = [ 1 , 2 , 2 , 5 , 6 , 8 , 3 ]
+tech = ["Python"]
+# tech = tech*3
+# # print(tec)
+# print(tech)
+
+# print(type(names))
+# print(type(nums))
+
+# print(names)
+# print(nums)
+
+# print(names[0])
+# print(names[4])
+# print(names[3])
+
+# names[3] = "march"  #mutation
+# print(names[3])
+# print(names)
+
+# status = "march" in names
+# print(status)
+# tatus = 5 not in nums
+# print(tatus)
+
+# del names 
+# print(names)
+
+# print(nums[0:4])
+# print(nums[0:4:2])
+# print(nums[-5:-1])
+# print(nums[::-1])
+
+# print(names)
+# names.append("Khan")
+# print(names)
+# names.pop()
+# print(names)
+# del names[1]
+# print(names)
+# names.insert(2 , 25)
+# print(names)
+
+
+# print(max(nums))
+# print(min(nums))
+# print(len(names))
+# print(nums)
+# nSort = nums.sort()
+# print(nums)
+# print(names)
+# names.reverse()
+# print(names)
+# print(nums.count(5))
+# conct = names+nums
+#print(conct)
+# print(names)
+# print(nums)
+# names.extend(nums)
+# print(names)
+# print(nums)
+
+# y = []
+# for i in  range(10):
+# 	y.append(i*2)
+# print(y)
+
+
+# yy = [i*2 for i in range(10)]
+# print(yy)
+
+# threeMuls = [i for i in range(40) if(i%3==0)]
+# print(threeMuls)
